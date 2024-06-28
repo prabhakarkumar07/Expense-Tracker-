e87d8b182f3f7f16141758fe1af082f461a1848f
PrabhakarKumar <prabhakarkumarekc477@gmail.com>
2024-06-28 16:06:53 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <Header />
+  <div class="container">
+    <Balance :total="+total" />
+    <Income :income="+income" :expense="+expense" />
+    <Transaction
+      :transactions="transactions"
+      @transactionDeleted="handleTransactionDeleted"
+    />
+    <AddTransaction @transactionSubmited="handleTransactionSubmited" />
+  </div>
+</template>
+
+<script setup>
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import Income from "./components/Income.vue";
+import Transaction from "./components/Transaction.vue";
+import AddTransaction from "./components/AddTransaction.vue";
+
+import { ref, computed, onMounted } from "vue";
+import { useToast } from "vue-toastification";
+
+const toast = useToast();
+
+const transactions = ref([]);
+//saving to local storage
+onMounted(() => {
+  const savedTransactions = JSON.parse(localStorage.getItem("transactions"));
+
+  if (savedTransactions) {
+    transactions.value = savedTransactions;
+  }
+});
+// Get total
+const total = computed(() => {
+  return transactions.value.reduce(
+    (acc, transaction) => acc + transaction.amount,
+    0
+  );
+});
+
+// Get income
+const income = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0)
+    .toFixed(2);
+});
+
+// Get expense
+const expense = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0)
+    .toFixed(2);
+});
+
+// Add transaction
+const handleTransactionSubmited = (transactionData) => {
+  transactions.value.push({
+    id: genrateId(),
+    text: transactionData.text,
+    amount: transactionData.amount,
+  });
+  saveTransactionsToLocalStorage();
+  toast.success("Transaction Added");
+};
+
+// Generate unique id
+const genrateId = () => {
+  return Math.floor(Math.random() * 10000000000);
+};
+
+// Delete Transaction
+const handleTransactionDeleted = (id) => {
+  transactions.value = transactions.value.filter(
+    (transaction) => transaction.id !== id
+  );
+  saveTransactionsToLocalStorage();
+  toast.success("Transaction Deleted");
+};
+
+//save to local storage
+const saveTransactionsToLocalStorage = () => {
+  localStorage.setItem("transactions", JSON.stringify(transactions.value));
+};
+</script>
---
+++ src/components/Transaction.vue
@@ -0,0 +1,93 @@
+<template>
+  <h3>History</h3>
+  <div id="list" class="list">
+    <li
+      v-for="transaction in visibleTransactions"
+      v-bind:key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>{{ transaction.amount }} </span
+      ><button
+        class="delete-btn"
+        @click="deleteTransaction(transaction.id)"
+        @click.enter="deleteTransaction(transaction.id)"
+      >
+        X
+      </button>
+    </li>
+  </div>
+  <div class="btn-container">
+    <button v-if="showViewMore" @click="toggleView" class="view-more-btn">
+      View More
+    </button>
+    <button v-if="showViewLess" @click="toggleView" class="view-less-btn">
+      View Less
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from "vue";
+import { defineProps, defineEmits } from "vue";
+
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["transactionDeleted"]);
+const deleteTransaction = (id) => {
+  emit("transactionDeleted", id);
+};
+
+const showAll = ref(false);
+
+const visibleTransactions = computed(() => {
+  return showAll.value ? props.transactions : props.transactions.slice(0, 4);
+});
+
+const showViewMore = computed(() => {
+  return props.transactions.length > 4 && !showAll.value;
+});
+
+const showViewLess = computed(() => {
+  return props.transactions.length > 4 && showAll.value;
+});
+
+const toggleView = () => {
+  showAll.value = !showAll.value;
+};
+</script>
+
+<style scoped>
+.minus {
+  color: red;
+}
+
+.plus {
+  color: green;
+}
+.btn-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.view-more-btn,
+.view-less-btn {
+  background-color: #007bff;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  cursor: pointer;
+  border-radius: 5px;
+  margin: 0 5px;
+}
+
+.view-more-btn:hover,
+.view-less-btn:hover {
+  background-color: #0056b3;
+}
+</style>
